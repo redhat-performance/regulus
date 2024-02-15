543a97bf788cc2600da5895d29a26e727489bbc1
HughNhan <hnhan@redhat.com>
2024-02-15 15:37:05 -0500

    point SRIOV root to the bastion KNI

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,7 @@ if [ -z "$REG_ROOT" ]; then
 	export REG_ROOT=$(pwd)
 	export PATH=$PATH:/$REG_ROOT/bin
 fi
+
+if [ !  -x "$(command -v reg-run)" ]; then
+	echo please unset REG_ROOT and source bootstrap.sh again.
+fi
