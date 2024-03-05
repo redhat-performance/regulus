697cb2d9d754698df493cc12377c94bbc1e2aef7
root <root@f07-h03-000-1029p.rdu2.scalelab.redhat.com>
2024-03-05 22:11:33 +0000

    delete SETUP_GROUP reg_expand.sh to prevent run-all from execute them

--- SETUP_GROUP/PAO/CLEANUP/reg_expand.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Nothing to expand. This is a dummy dir to support oc/kubectl
-
-REG_ROOT=${REG_ROOT:-/root/REGULUS}
-touch foo
-
--- SETUP_GROUP/SRIOV/INSTALL/reg_expand.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Dummy reg_expand.sh. 
-
-REG_ROOT=${REG_ROOT:-/root/REGULUS}
-touch foo
-
