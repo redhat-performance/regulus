f8f2fc3c817914b933898a378b8d8c49cba9c458
HughNhan <hnhan@redhat.com>
2024-09-03 15:18:57 -0400

    correct 4.14 and 4.15 config

--- IPsec-FULL-config/cleanup.sh
+++ IPsec-FULL-config/cleanup.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 #
-# Cleanup/remove/disable IPsec internal node-to-node traffic
+# Remove IPsec for local traffic ONLY.
 #
 
-oc patch networks.operator.openshift.io cluster --type=merge \
-    -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":null}}}}'
+source ./functions.sh
 
-# EOF
+channel="$(get_ocp_channel)"
+if [ "$channel" == "4.14"  ] ; then
+    oc patch networks.operator.openshift.io/cluster --type=json -p='[{"op":"remove", "path":"/spec/defaultNetwork/ovnKubernetesConfig/ipsecConfig"}]'
+else
+    # 4.15 and later
+    oc patch networks.operator.openshift.io cluster --type=merge -p '{ "spec":{ "defaultNetwork":{ "ovnKubernetesConfig":{ "ipsecConfig":{ "mode":"Disabled" }}}}}'
+fi
+
+# debug: oc get networks.operator.openshift.io cluster -o yaml
+
+
--- IPsec-FULL-config/install.sh
+++ IPsec-FULL-config/install.sh
@@ -7,10 +7,10 @@ source ./functions.sh
 
 channel="$(get_ocp_channel)"
 if [ "$channel" == "4.14"  ] ; then
-    oc patch networks.operator.openshift.io cluster --type=merge  -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{ }}}}}'
+    oc patch networks.operator.openshift.io cluster --type=merge -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{ }}}}}'
 else
-    oc patch networks.operator.openshift.io cluster --type=merge -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{"mode": "Full"}}}}}'
-
+    # 4.15 and later on
+    oc patch networks.operator.openshift.io cluster --type=merge -p '{ "spec":{ "defaultNetwork":{ "ovnKubernetesConfig":{ "ipsecConfig":{ "mode":"full" }}}}}'
 fi
 
 # debug: oc get networks.operator.openshift.io cluster -o yaml
