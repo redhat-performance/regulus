1a496656971cd71dde137f06d2760b6edd1cdf4a
HughNhan <hnhan@redhat.com>
2024-09-03 10:18:07 -0400

    fix Ipsec config for 4.14

--- IPsec-FULL-config/install.sh
+++ IPsec-FULL-config/install.sh
@@ -3,9 +3,17 @@
 # Install IPsec for local traffic ONLY. i.e mode=Full, and without additional config for External traffic
 #
 
-echo oc patch networks.operator.openshift.io cluster --type=merge \
-    -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{"mode": "Full"}}}}}'
+source ./functions.sh
+
+channel="$(get_ocp_channel)"
+if [ "$channel" == "4.14"  ] ; then
+    echo oc patch networks.operator.openshift.io cluster --type=merge  -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{ }}}}}'
+else
+    echo "Regulus does not support $channel IPsec config"
+fi
 
 #
-# debug: oc -n openshift-ovn-kubernetes rsh ovnkube-node-<XXXXX> ovn-nbctl --no-leader-only get nb_global . ipsec
-#
+# 4.15 config 
+# oc patch networks.operator.openshift.io cluster --type=merge -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{"mode": "Full"}}}}}'
+
+
