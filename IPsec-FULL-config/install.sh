a4400d864bde1e0bb0d99da57b7d9883c8923cb3
HughNhan <hnhan@redhat.com>
2024-09-09 14:23:47 -0400

    type Full vs full

--- IPsec-FULL-config/install.sh
+++ IPsec-FULL-config/install.sh
@@ -10,7 +10,7 @@ if [ "$channel" == "4.14"  ] ; then
     oc patch networks.operator.openshift.io cluster --type=merge -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{ }}}}}'
 else
     # 4.15 and later on
-    oc patch networks.operator.openshift.io cluster --type=merge -p '{ "spec":{ "defaultNetwork":{ "ovnKubernetesConfig":{ "ipsecConfig":{ "mode":"full" }}}}}'
+    oc patch networks.operator.openshift.io cluster --type=merge -p '{ "spec":{ "defaultNetwork":{ "ovnKubernetesConfig":{ "ipsecConfig":{ "mode":"Full" }}}}}'
 fi
 
 # debug: oc get networks.operator.openshift.io cluster -o yaml
