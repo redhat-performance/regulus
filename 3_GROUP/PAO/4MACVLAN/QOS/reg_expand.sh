aa08277eea728ad0ef1156e40fa43e82f00ac3f4
HughNhan <hnhan@redhat.com>
2024-12-18 13:48:01 -0500

    add MACVLAN mbench testcases

---
+++ 3_GROUP/PAO/4MACVLAN/QOS/reg_expand.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# mbench PAO, IPv4, MACVLAN, QoS
+
+REG_ROOT=${REG_ROOT:-/root/REGULUS}
+REG_TEMPLATES=${REG_ROOT}/templates/mbench
+REG_COMMON=${REG_ROOT}/templates/common
+MANIFEST_DIR=./
+
+source $REG_ROOT/lab.config    		# for worker node names
+source ${REG_ROOT}/system.config	# for MCP
+
+# generate run.sh with custom-param "node-config"
+export TPL_NODE_CONF="node-config"
+envsubst '$MCP,$TPL_NODE_CONF' < ${REG_TEMPLATES}/run.sh.template > ${MANIFEST_DIR}/run.sh
+
+# generate run-3types.sh. No custom params
+export TPL_MACVLAN=1
+envsubst '$TPL_MACVLAN'  < ${REG_TEMPLATES}/run-3types-14pods.sh.template > ${MANIFEST_DIR}/run-3types.sh
+
+# generate node-config w/o custom resources. 
+export TPL_RESOURCES=',resources:default:$pwd/resource-static-Ncpu.json'
+envsubst '$MCP,$TPL_RESOURCES,$TPL_MACVLAN' < ${REG_TEMPLATES}/base-pao-node-config.template > ${MANIFEST_DIR}/node-config
+
+# generate custom CPU resources and set to 2 CPUs
+export TPL_NUMCPUS=2
+envsubst '$TPL_NUMCPUS' < ${REG_COMMON}/resource-static-Ncpu.json.template > ${MANIFEST_DIR}/resource-static-Ncpu.json
+
+# generate annotation. 
+envsubst '' < ${REG_COMMON}/annotations-macvlan-pao-qos.json.template  > ${MANIFEST_DIR}/annotations.json
+
+# generate placement 
+envsubst '' < ${REG_TEMPLATES}/standard-32pairs.placement.template  > ${MANIFEST_DIR}/pairs.placement
+
+# generate mv-params
+export TPL_INTF=net1
+export TPL_IPV=4
+envsubst '$TPL_INTF,$TPL_IPV' < ${REG_TEMPLATES}/iperf-mv-params.json.template >  ${MANIFEST_DIR}/iperf-mv-params.json
+envsubst '$TPL_INTF,$TPL_IPV' < ${REG_TEMPLATES}/uperf-mv-params.json.template >  ${MANIFEST_DIR}/uperf-mv-params.json
+
+# generta tools params. No custom params
+envsubst '' < ${REG_COMMON}/tool-params.json.template >  ${MANIFEST_DIR}/tool-params.json
+cp ${REG_COMMON}/securityContext.json.template  ${MANIFEST_DIR}/securityContext.json
+
+# generate worker node mapping
+export TPL_WORKER=$OCP_WORKER_0
+envsubst '$TPL_WORKER' < ${REG_TEMPLATES}/nodeSelector-worker-n.json.template >  ${MANIFEST_DIR}/nodeSelector-worker-0.json
+
+export TPL_WORKER=$OCP_WORKER_1
+envsubst '$TPL_WORKER' < ${REG_TEMPLATES}/nodeSelector-worker-n.json.template >  ${MANIFEST_DIR}/nodeSelector-worker-1.json
+
+export TPL_WORKER=$OCP_WORKER_2
+envsubst '$TPL_WORKER' < ${REG_TEMPLATES}/nodeSelector-worker-n.json.template >  ${MANIFEST_DIR}/nodeSelector-worker-2.json
+
+# done
