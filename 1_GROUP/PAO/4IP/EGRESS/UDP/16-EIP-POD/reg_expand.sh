8a71e1eeeabed47984fcacf9165113fa3acde8cb
HughNhan <hnhan@redhat.com>
2025-01-14 19:36:17 -0500

    add EIP test cases and fix run.sh INGRESS/EGRESS nodeSelector

---
+++ 1_GROUP/PAO/4IP/EGRESS/UDP/16-EIP-POD/reg_expand.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# uperf PAO,IPv4,EGRESS EIP,2 Pods
+
+REG_ROOT=${REG_ROOT:-/root/REGULUS}
+REG_TEMPLATES=${REG_ROOT}/templates/uperf
+REG_COMMON=${REG_ROOT}/templates/common
+MANIFEST_DIR=./
+
+export TPL_SCALE_UP_FACTOR=8
+export TPL_TOPO=egress
+export TPL_PAO=1
+
+envsubst '$MCP,$TPL_PAO,$TPL_SCALE_UP_FACTOR,$TPL_TOPO' < ${REG_TEMPLATES}/run.sh.template > ${MANIFEST_DIR}/run.sh
+export TPL_INTF=$REMOTE_HOST_INTF     # defined in jobs.config
+envsubst '$TPL_INTF' <  ${REG_TEMPLATES}/udp-mv-params.json.template >  ${MANIFEST_DIR}/mv-params.json
+
+cp ${REG_COMMON}/tool-params.json.template  ${MANIFEST_DIR}/tool-params.json
+cp ${REG_COMMON}/annotations-pao.json.template  ${MANIFEST_DIR}/annotations.json
