8ac7de5ff4e64856b28a5db3659ea316805d31a7
root <root@e30-h03-000-r640.rdu2.scalelab.redhat.com>
2025-07-21 19:42:43 +0000

    re-activate SRIOV-config full, enhance reg-smart-config

--- SRIOV-config/UNIV/full-install.sh
+++ SRIOV-config/UNIV/full-install.sh
@@ -16,7 +16,7 @@
 source ${REG_ROOT}/lab.config
 source ${REG_ROOT}/SRIOV-config/config.env
 
-SINGLE_STEP=${SINGLE_STEP:-true}
+SINGLE_STEP=${SINGLE_STEP:-false}
 PAUSE=${PAUSE:-false}
 
 if [ $PAUSE == true ]; then
@@ -36,7 +36,7 @@ if [ -z "$WORKER_LIST"  ]; then
     echo no WORRKER_LIST
     exit
 fi
-echo Use mcp $MCP 
+echo $LINENO Use mcp $MCP 
 
 export OCP_CHANNEL=$(oc get packagemanifest sriov-network-operator -n openshift-marketplace -o json | jq -r '.status.channels[0].name')
 
@@ -52,28 +52,29 @@ function install_sriov_operator {
     # install SRIOV operator
     # skip if sriov operator subscription already exists 
     if oc get Subscription sriov-network-operator-subscription -n openshift-sriov-network-operator &>/dev/null; then 
-        echo "SRIOV Operator already installed: done"
+        echo $LINENO  "SRIOV Operator already installed: done"
     else
         #// Installing SR-IOV Network Operator done
         echo "Installing SRIOV Operator ..."
         envsubst '$OCP_CHANNEL' < templates/sub-sriov.yaml.template > ${MANIFEST_DIR}/sub-sriov.yaml
-        ECHO oc create -f ${MANIFEST_DIR}/sub-sriov.yaml
+        oc create -f ${MANIFEST_DIR}/sub-sriov.yaml
         echo "install SRIOV Operator: done"
-        ECHO wait_pod_in_namespace openshift-sriov-network-operator
+        wait_pod_in_namespace openshift-sriov-network-operator
         # give it a little delay. W/o delay we could encounter error on the next command.
-        ECHO sleep 10
+        sleep 10
     fi
 
     ### install SRIOV operator config. Required since 4.18
-    if oc get sriovoperatorconfig  -n openshift-sriov-network-operator &>/dev/null; then 
-        echo "SRIOV Operator config already installed: done"
-    else
-        echo "Installing SRIOV Operator config ..."
-        envsubst  < templates/sriov-operator-config.yaml.template > ${MANIFEST_DIR}/sriov-operator-config.yaml
-        ECHO oc create -f ${MANIFEST_DIR}//sriov-operator-config.yaml
+	resource_count=$(oc get sriovoperatorconfig -n openshift-sriov-network-operator --no-headers 2>/dev/null | wc -l)
+	if [ "$resource_count" -gt 0 ]; then
+        echo $LINENO "SRIOV Operator config already installed: done"
+	else
+        echo $LINENO "Installing SRIOV Operator config ..."
+        envsubst < templates/sriov-operator-config.yaml.template > ${MANIFEST_DIR}/sriov-operator-config.yaml
+        oc create -f ${MANIFEST_DIR}/sriov-operator-config.yaml
         echo "install SRIOV Operator: done"
-        ECHO sleep 10
-    fi
+        sleep 10
+	fi
 
 }
 
@@ -89,7 +90,7 @@ function configure_mcp {
         echo "create mcp for ${MCP}  ..."
         mkdir -p ${MANIFEST_DIR}
         envsubst < templates/mcp-regulus-vf.yaml.template > ${MANIFEST_DIR}/mcp-regulus-vf.yaml
-        ECHO oc create -f ${MANIFEST_DIR}/mcp-regulus-vf.yaml
+        oc create -f ${MANIFEST_DIR}/mcp-regulus-vf.yaml
         echo "create mcp for ${MCP} done"
     fi
 }
@@ -100,12 +101,15 @@ prompt_continue
 # Create a new MCP, but if cluster is SNO or 3-node compact, only mcp master has nodes, and we must use mcp master.
 #                   Higher level should have set setting.env with "MCP=master" to indicate.
 if [ "${MCP}" != "master" ]; then
-    ECHO configure_mcp
+    configure_mcp
 else
     echo "Will use master mcp"
     # Put a label on master mcp so that new MCs can select.
-    ECHO oc label --overwrite mcp master machineconfiguration.openshift.io/role=master
+    oc label --overwrite mcp master machineconfiguration.openshift.io/role=master
 fi
+
+echo $LINENO "next is label node in ${WORKER_LIST} with ${MCP} label"
+prompt_continue
 
 # step 3 - label nodes that needs SRIOV
 
@@ -113,7 +117,8 @@ function add_label {
     if [ "$MCP}" != "master" ]; then
         for NODE in $WORKER_LIST; do
             echo label $NODE with $MCP
-            ECHO oc label --overwrite node ${NODE} node-role.kubernetes.io/${MCP}=""
+            oc label --overwrite node ${NODE} node-role.kubernetes.io/${MCP}=""
+            prompt_continue
         done
     else
         echo "Cluster has no workers. No need to label master nodes"
@@ -121,8 +126,14 @@ function add_label {
 }
 add_label
 
+echo $LINENO "next wait_mcp"
+prompt_continue
+
 # MCP goes to UPDATING after add label
 wait_mcp
+
+echo $LINENO "next is add realloc to machineconfig "
+prompt_continue
 
 # add this if necessary
 function add_mc_realloc {
@@ -131,7 +142,7 @@ function add_mc_realloc {
     else
         echo "create mc mc-realloc.yaml ..."
         envsubst < templates/mc-realloc.yaml.template > ${MANIFEST_DIR}/mc-realloc.yaml
-        ECHO oc create -f ${MANIFEST_DIR}/mc-realloc.yaml
+        oc create -f ${MANIFEST_DIR}/mc-realloc.yaml
         echo "create mc-realloc.yaml: done"
     fi
 }
@@ -144,7 +155,7 @@ fi
 
 
 if [ $PAUSE == true ]; then
-  ECHO pause_mcp
+  pause_mcp
 fi
 
 # step 5  - SriovNetworkNodePolicy. Tell it what SRIOV devices (mlx, 710 etc) to be activated.
@@ -170,18 +181,18 @@ function config_SriovNetworkNodePolicy {
     # step 2 - apply
 
     for NODE in ${WORKER_LIST}; do
-        ECHO oc label --overwrite node $NODE feature.node.kubernetes.io/network-sriov.capable=true
+        oc label --overwrite node $NODE feature.node.kubernetes.io/network-sriov.capable=true
     done
 
     if oc get SriovNetworkNodePolicy regulus-sriov-node-policy -n openshift-sriov-network-operator  2>/dev/null; then
         echo "SriovNetworkNodePolicy exists. Skip creation"
     else
         echo "create SriovNetworkNodePolicy ..."
-        ECHO oc create -f ${MANIFEST_DIR}/sriov-node-policy.yaml
+        oc create -f ${MANIFEST_DIR}/sriov-node-policy.yaml
         echo "create SriovNetworkNodePolicy: done"
         if [ $PAUSE == false ]; then
            # MCP may not go to UPDATING after apply SriovNetworkNodePolicy
-           ECHO wait_mcp
+           wait_mcp
         fi
         # !!!!! node reboot !!!!
     fi
@@ -205,8 +216,8 @@ function create_network {
         echo "SriovNetworkexists. Skip creation"
     else
         echo "create network-attachment-definition/ ..."
-        ECHO oc new-project ${MCP}  &> /dev/null
-        ECHO oc create -f ${MANIFEST_DIR}/net-attach-def.yaml
+        oc new-project ${MCP}  &> /dev/null
+        oc create -f ${MANIFEST_DIR}/net-attach-def.yaml
         echo "create NAD /net-attach-def.yaml  done"
     fi
 }
