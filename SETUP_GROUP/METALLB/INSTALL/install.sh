a46270ada07b755f4c5bd1764e5692e1649826fb
root <root@e30-h03-000-r640.rdu2.scalelab.redhat.com>
2025-07-21 23:52:36 +0000

    all *-config/INSTALL,CLEANUP to use /tmp/regulus

--- SETUP_GROUP/METALLB/INSTALL/install.sh
+++ SETUP_GROUP/METALLB/INSTALL/install.sh
@@ -5,9 +5,7 @@ if ! ssh $REG_KNI_USER@$REG_OCPHOST "kubectl get node &>/dev/null"; then
     echo "ERROR: check testbed"
     exit 1
 fi
-# remote REG_ROOT can be different i.e root vs kni. Extract the regulus dir part
-reg_dir=$(basename "$REG_ROOT")
 
-ssh $REG_KNI_USER@$REG_OCPHOST "cd $reg_dir && source bootstrap.sh && cd GW-config/METALLB/ && bash install.sh "  
+ssh $REG_KNI_USER@$REG_OCPHOST "cd $REG_DIR && source bootstrap.sh && cd GW-config/METALLB/ && bash install.sh "  
 
 # Done
