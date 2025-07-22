00a3510937825a28959e688819e4ae42debaad70
root <root@e30-h03-000-r640.rdu2.scalelab.redhat.com>
2025-07-22 15:03:30 +0000

    macvlan NAD with and w/o IPv6 support

--- MACVLAN-config/install.sh
+++ MACVLAN-config/install.sh
@@ -14,8 +14,14 @@ parse_args $@
 
 mkdir -p ${MANIFEST_DIR}/
 
+if oc get network.config.openshift.io cluster -o jsonpath='{.spec.clusterNetwork[*].cidr}' | grep -q ":"; then
+    export REG_IPV6_RANGE=',{"range_v6": "2001::00/64"}'
+else
+    export REG_IPV6_RANGE=""
+fi
+
 function create_nad {
-    envsubst < templates/net-attach-def.yaml.template > ${MANIFEST_DIR}/net-attach-def.yaml
+    envsubst  < templates/net-attach-def.yaml.template > ${MANIFEST_DIR}/net-attach-def.yaml
 
     echo "Defer NAD creation to run.sh"
     return # lately we create NAD per test. So no need to create now
