b8a8ffcd2cc69e5b0f7128b388ffbae6f4a2dd69
HughNhan <hnhan@redhat.com>
2025-10-09 21:33:30 -0400

    add DPU-mode test cases and supports

---
+++ 1_GROUP/PAO/4DPU/INTER-NODE/TCP/2-POD-GU/reg_expand.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# uperf PAO,DPU,IPv4,INTER_NODE,2 Pods GU
+
+REG_ROOT=${REG_ROOT:-/root/REGULUS}
+REG_TEMPLATES=${REG_ROOT}/templates/uperf
+REG_COMMON=${REG_ROOT}/templates/common
+MANIFEST_DIR=./
+
+export TPL_SCALE_UP_FACTOR=1
+export TPL_QOS=static
+export TPL_TOPO=internode
+export TPL_PAO=1
+export TPL_DPF=1
+
+envsubst '$TPL_QOS,$TPL_PAO,$TPL_DPF,$TPL_SCALE_UP_FACTOR,$TPL_TOPO' < ${REG_TEMPLATES}/run.sh.template > ${MANIFEST_DIR}/run.sh
+export TPL_INTF=eth0
+export TPL_IPV=4
+envsubst '$TPL_INTF,$TPL_IPV' <  ${REG_TEMPLATES}/tcp-mv-params.json.template >  ${MANIFEST_DIR}/mv-params.json
+
+cp ${REG_COMMON}/tool-params.json.template  ${MANIFEST_DIR}/tool-params.json
+cp ${REG_COMMON}/securityContext.json.template  ${MANIFEST_DIR}/securityContext.json
+cp ${REG_COMMON}/annotations-pao-qos.json.template  ${MANIFEST_DIR}/annotations-pao-qos.json
+cp ${REG_COMMON}/annotations-dpf.json.template  ${MANIFEST_DIR}/annotations.json
+cp ${REG_COMMON}/resource-dpf.json.template ${MANIFEST_DIR}/resource-dpf.json
+
