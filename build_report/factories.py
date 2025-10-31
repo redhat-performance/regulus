06dce3e132d4367d4c100cc32370eaab624f0fe8
HughNhan <hnhan@redhat.com>
2025-10-30 21:50:18 -0400

    make clickable file column in the csv

--- build_report/factories.py
+++ build_report/factories.py
@@ -90,13 +90,13 @@ def create_html_orchestrator(schema_version=SchemaVersion.V2_0):
         output_generator=HtmlOutputGenerator()  # HTML instead of JSON
     )
 
-def create_multi_format_orchestrator(formats=['json', 'html']):
+def create_multi_format_orchestrator(formats=['json', 'html'], base_url=''):
     """Create orchestrator with multiple output formats."""
     from .output.generators import EnhancedMultiFormatOutputGenerator
     
     schema_manager = SchemaManager(SchemaVersion.V2_0)
-    multi_generator = EnhancedMultiFormatOutputGenerator(schema_manager)
-    
+    multi_generator = EnhancedMultiFormatOutputGenerator(schema_manager, base_url=base_url)
+
     # Enable specified formats
     for fmt in formats:
         multi_generator.enable_format(fmt)
