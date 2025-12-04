e0c4cba01ce480b827089646c1f8ae847079c47e
HughNhan <hnhan@redhat.com>
2025-12-03 20:50:50 -0500

    fix relative path :/home/hnhan/TRAFFGEN/latency-regulus/DPDK-config

--- SETUP_GROUP/DPDK/INSTALL/init.sh
+++ SETUP_GROUP/DPDK/INSTALL/init.sh
@@ -11,7 +11,7 @@ ssh $REG_KNI_USER@$REG_OCPHOST "cd $REG_DIR && source bootstrap.sh && cd DPDK-co
 if [[ -n "$REM_DPDK_CONFIG" && "$REM_DPDK_CONFIG" == "true" ]]; then
    echo CMD: ssh root@$TREX_HOSTS "cd $REG_DIR && source bootstrap.sh && cd DPDK-config/common && bash device-info.sh out.json"  
    ssh root@$TREX_HOSTS "cd $REG_DIR && source bootstrap.sh && cd DPDK-config/common && bash device-info.sh out.json"  
-   scp root@$TREX_HOSTS:~/$REG_DIR/DPDK-config/common/out.json  trex-device-info.json
+   scp root@$TREX_HOSTS:$REG_DIR/DPDK-config/common/out.json  trex-device-info.json
 
 fi
 
