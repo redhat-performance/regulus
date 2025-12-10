ccf25577f2f9137e82461d06b289b4fe105dd4c1
Pradipta Kumar Sahoo <pradiptapks@gmail.com>
2025-12-11 02:54:12 +0530

    Fix: Enhancement of Trex Bench-Trafficgen Profile for Low-Latency Performance Optimization (#14)
    * Fix: Enhancement of Trex Bench-Trafficgen Profile for Low-Latency Performance Optimization
    
    TRex traffic generator client profile: continuous 100% load with warmup/teaching packets, fixed random seed, rate/runtime tolerances, max loss threshold, and search/control parameters configured as part of RFC2544 latency improvements
    
    * Update patch with additional fixes
    
    * Create Seprate Group for Trafficgen Profiles like LATENCY and MAC-MODE
    
    * Refining the trafficgen template

---
+++ DPDK_GROUP/TRAFFICGEN/GENERAL/reg_expand.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# trafficgen 4CPUs, "mac" forwarding mode.
+
+REG_ROOT=${REG_ROOT:-/root/REGULUS}
+REG_TEMPLATES=${REG_ROOT}/templates/trafficgen
+REG_COMMON=${REG_ROOT}/templates/common
+source $REG_ROOT/lab.config
+
+export TPL_MLXSUP=${TPL_MLXSUP:-off}
+export TPL_SW_MODE=${TPL_SW_MODE:-on}
+if [ "$TREX_DPDK_NIC_MODEL"  == "CX6"  ]; then
+    TPL_MLXSUP=on
+    TPL_SW_MODE=off
+fi
+
+MANIFEST_DIR=./
+
+export TPL_FWD_MODE=mac
+export TPL_NUMCPUS=4
+envsubst '$MCP' < ${REG_TEMPLATES}/run.sh.template > ${MANIFEST_DIR}/run.sh
+envsubst '$TPL_MLXSUP,$TPL_SW_MODE,$TPL_FWD_MODE' <  ${REG_TEMPLATES}/general/trex-txrx/mv-params.json.template >  ${MANIFEST_DIR}/mv-params.json
+envsubst '$TPL_NUMCPUS' <  ${REG_TEMPLATES}/resources.json.template >  ${MANIFEST_DIR}/resources.json
+envsubst '' <  ${REG_TEMPLATES}/annotations.json.template  > ${MANIFEST_DIR}/annotations.json
+
+cp ${REG_COMMON}/tool-params.json.template  ${MANIFEST_DIR}/tool-params.json
+cp ${REG_COMMON}/securityContext.json.template  ${MANIFEST_DIR}/securityContext.json
--- DPDK_GROUP/TRAFFICGEN/LATENCY/reg_expand.sh
+++ DPDK_GROUP/TRAFFICGEN/LATENCY/reg_expand.sh
@@ -7,7 +7,7 @@ REG_COMMON=${REG_ROOT}/templates/common
 source $REG_ROOT/lab.config
 
 export TPL_MLXSUP=${TPL_MLXSUP:-off}
-export TPL_SW_MODE=${TPL_SW_MODE:-on}
+export TPL_SW_MODE=${TPL_SW_MODE:-off}
 if [ "$TREX_DPDK_NIC_MODEL"  == "CX6"  ]; then
     TPL_MLXSUP=on
     TPL_SW_MODE=off
@@ -18,9 +18,10 @@ MANIFEST_DIR=./
 export TPL_FWD_MODE=mac
 export TPL_NUMCPUS=4
 envsubst '$MCP' < ${REG_TEMPLATES}/run.sh.template > ${MANIFEST_DIR}/run.sh
-envsubst '$TPL_MLXSUP,$TPL_SW_MODE,$TPL_FWD_MODE' <  ${REG_TEMPLATES}/mv-params.json.template >  ${MANIFEST_DIR}/mv-params.json
+envsubst '$TPL_MLXSUP,$TPL_SW_MODE,$TPL_FWD_MODE' <  ${REG_TEMPLATES}/latency/trex-txrx-profile/mv-params.json.template >  ${MANIFEST_DIR}/mv-params.json
 envsubst '$TPL_NUMCPUS' <  ${REG_TEMPLATES}/resources.json.template >  ${MANIFEST_DIR}/resources.json
 envsubst '' <  ${REG_TEMPLATES}/annotations.json.template  > ${MANIFEST_DIR}/annotations.json
+envsubst '' <  ${REG_TEMPLATES}/latency/trex-txrx-profile/trafficgen.profile.template  > ${MANIFEST_DIR}/trafficgen.profile
 
 cp ${REG_COMMON}/tool-params.json.template  ${MANIFEST_DIR}/tool-params.json
 cp ${REG_COMMON}/securityContext.json.template  ${MANIFEST_DIR}/securityContext.json
